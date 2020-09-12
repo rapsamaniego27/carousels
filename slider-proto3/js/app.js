b0e6256836b062a92cf82aab98b9e91f0cc47e6f
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-09-12 18:35:38 +0800

    created prototype of Dulas slider library

---
+++ slider-proto3/js/app.js
@@ -0,0 +1,13 @@
+/* Code javascript here */
+const slider = new Dulas({
+ sliderEl: document.querySelector('.slide-row'),
+
+ controls:{
+  nextEl:document.querySelector('#arrowRight'),
+  prevEl:document.querySelector('#arrowLeft')
+ },
+
+ autoplay:false
+});
+
+
